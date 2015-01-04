620c8b3d3bbcaf60d1e7cdbbe29f46584ccdedfa
Bjorn Burton <bjorn@mm.st>
2015-01-04 15:55:00 -0500

    changed char flag to bit flags for interrups
    moved int flag resets into their respective blocks
    
    changed f_interrupt to f_state to better reflect the planned use. Added ALARM flag.
    
    moved hold-off action to its own function. Disabled the wave comparaitor interrupt durring the hold-of period.
    
    added an alarm armdelay parameter
    
    added siren control function
    
    added flags and logic to arm and alarm
    
    added chirp function for alarm
    
    improved init function names
    
    improved comment
    
    built the hex file

--- ased.h
+++ ased.h
@@ -1,6 +1,6 @@
 /**************************************
 ASED
-Version 1.0.0
+Version 1.1.0
 Ancillary Service Electric Detector
 2015-01-01
 Bjorn Burton
@@ -19,30 +19,53 @@ Just for fun.
 
 /**** Defines *****/
  /* LED port */
-# define LED_RED PORTB1
+# define LED_RED PORTB1  // Trinket's LED and pin #2
 # define LED_RED_DD DDB1
+
+ /* siren port */
+# define SIREN PORTB0 // Trinket's LED and pin #0
+# define SIREN_DD DDB0
+
  /* Bool*/
 # define ON 1
 # define OFF 0
 # define SET 1
 # define CLEAR 0
 
+ /* Flags for f_state */
+# define NOWAVES 2 // no ASE detected for some time 
+# define WAVES   1 // ASE detected
+# define ARM     0 // ARM for Alarm
+
  /* Parameters */
-# define WAVETHRESHOLD 15 // wave count debounce; 15 waves is "good"
+# define WAVETHRESHOLD 15
+/* Number of wave before considering the service on. Range to 255 */
 /* About 250 ms. Don't take too long or time will run out */
 
-# define TIMESTART 12     // preset for the timer counter
+# define TIMESTART 12     // preset for the timer counter. Range to 255 */
 /* Prescaler is set to clk/16484. 
 0.5 seconds *(8e6/16384) is 244.14.
 256-244 = 12, leaving 500 ms to time-out */
 
+# define WAVEHOLDOFFTIME 100 // Range to 255
+/* hold-off time in us for wave detection */
+
+# define ARMTHRESHOLD 60 // Range to 65535
+/* alarm arm delay in nowave counts*/
+
+/* chirp parameters for alarm */
+# define CHIRPLENGTH 7 // number of waves long
+# define CHIRPPERIOD 200 // number of waves long
+
 
 /**** Function Declarations ****/
 void ledcntl(char state);
-void initTimerCounter1(void); // sets up timer
-void initComparator(void); // sets up comparator
+void sirencntl(char state); //alarm siren control
+void chirp(char state); // alarm siren modulation
+void initnowavetimer(void); // sets up timer
+void initwavedetector(void); // sets up comparator
+void waveholdoff(void); // for noise resistance...debounce
 
 /**** Global variables ****/
-volatile char f_overflow = CLEAR; 
-volatile char f_sinewave = CLEAR; 
+volatile unsigned char f_state = 0x00; 
 
